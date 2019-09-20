795cd00c8d751c3f315691ca20798746e9425ade
appledore22 <52023866+appledore22@users.noreply.github.com>
2019-09-20 20:03:07 +0530

    Add files via upload

---
+++ wakeup.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+time="$(date +'%I:%M %p')"
+t="$(date +'%H')"
+if [ $t -le 12 ]
+then
+    echo "\nGood Morning Sir\n"
+    echo "It's $time"
+fi
+
+if [ $t -gt 12 -a $t -lt 17 ]
+then
+    echo "\nGood Afternoon Sir\n"
+    echo "It's $time"
+fi
+
+if [ $t -ge 17 ]
+then
+    echo "Good Evening Sir"
+    echo "It's $time"
+fi
+
+echo "\nWhat would you like to do :"
+echo "a - Start Questasim"
+echo "s - Open Google Chrome"
+echo "d - Open Appledore"
+
+  read INPUT_STRING
+  case $INPUT_STRING in
+	a)
+		echo "Starting Questasim"
+        /mnt/c/questasim_10.0b/win32/questasim.exe
+		;;
+	s)
+		echo "Opening Chrome"
+		/mnt/c/'Program Files (x86)'/Google/Chrome/Application/chrome
+		;;
+	d)
+		cd /mnt/d/Appledore
+        pwd
+        ;;
+    *)
+		echo "Sorry, I don't understand"
+		;;
+  esac
+
+    echo "See you around"
+
+
